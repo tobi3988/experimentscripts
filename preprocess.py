b2d4d0c32809b54a09523d5f56300fa81a485c38
Tobi <tobias.s.bachmann@gmail.com>
2018-04-12 16:49:19 +0200

    multipath

--- preprocess.py
+++ preprocess.py
@@ -2,18 +2,28 @@ import numpy as np
 
 
 def main():
+    # avgowd()
+    avgowd_multipath()
+
+
+def avgowd():
     rawdata = np.genfromtxt('experiments/logs/avgowd/metrics.csv', delimiter=',')
-    print(rawdata)
+    filtered_data = convert_to_seconds_and_delete_warmup(rawdata)
+    np.savetxt("out/avgowd_out.csv", filtered_data[:, [0, 1]], delimiter=",", fmt='%1.3f')
+
+
+def convert_to_seconds_and_delete_warmup(rawdata):
     end = rawdata[-1, 0]
-    print(end)
     start = end - 1200000  # -20min
-    print(start)
-    print(len(rawdata))
     rawdata[:, 0] = (rawdata[:, 0] - start) / 1000
-    print(rawdata[:, 0])
     filtered_data = rawdata[rawdata[:, 0] > 0]
-    print(filtered_data)
-    np.savetxt("out/avgowd_out.csv", filtered_data[:, [0, 1]], delimiter=",",fmt='%1.3f')
+    return filtered_data
+
+
+def avgowd_multipath():
+    rawdata = np.genfromtxt('experiments/logs/avgowd/multipath.csv', delimiter=',')
+    filtered_data = convert_to_seconds_and_delete_warmup(rawdata)
+    np.savetxt("out/avgowd_multipath_out.csv", filtered_data[:, [0, 1]], delimiter=",", fmt='%1.3f')
 
 
 if __name__ == '__main__':
